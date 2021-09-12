e97aa9d9751d4f7814f233ad8d6b9f4132fd0b83
cauzpema@gmail.com <cauzpema@gmail.com>
2021-09-12 22:03:07 +0200

    added config files

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  htmlWhitespaceSensitivity: 'css',
+}
